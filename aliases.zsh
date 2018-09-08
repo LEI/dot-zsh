64ba361238eb0054b1d4eca8799715c36556541b
LEI <guillaume.frichet@gmail.com>
2018-09-08 14:12:25 +0200

    al

--- aliases.zsh
+++ aliases.zsh
@@ -5,27 +5,9 @@
 alias reload='source ~/.zshrc'
 alias reload!='exec "$SHELL" -l'
 
-# More verbose
-alias cp="cp -v"
-alias ln="ln -v"
-alias mv="mv -v"
-alias rm="rm -v" # -i?
-
-# More colors
-alias grep="grep --color=auto"
-alias fgrep="fgrep --color=auto"
-alias egrep="egrep --color=auto"
-
-# Always continue getting partially-downloaded file
-alias wget="wget -c"
-
-# Misc shortcuts
-alias h="history"
-alias hgrep="history | grep"
-alias j="jobs"
-alias l="ls -lF"
-alias la="ls -lAF"
-alias path='echo $PATH | tr -s ":" "\n"'
+if [[ -f ~/.aliases ]]; then
+  source ~/.aliases
+fi
 
 # Help command
 autoload -Uz run-help
